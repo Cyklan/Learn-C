8641ba524b617b96d4f8d4b5d98b036dc6763d84
Cyklan <dev@cyklan.de>
2020-01-05 15:17:20 +0100

    Variables and Types

---
+++ variables_and_types/vat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // calculate with some integers
+    int a = 0;
+    int b = 1;
+    int c = 2;
+    int d = 3;
+    int e = 4;
+    a = b - c + d * e;
+    // Print a
+    printf("%d\n", a);
+
+    // add some floats
+    int x = 3;
+    float y = 4.5;
+    double z = 5.25;
+    float sum = x + y + z;
+    printf("The sum of x, y, and z is %f\n", sum);
+}
