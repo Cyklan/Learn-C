76439de7d14185c9457d7af65bb73431a4a3b1f8
Cyklan <dev@cyklan.de>
2020-01-05 15:23:37 +0100

    Arrays + formatting

---
+++ arrays/arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int numbers[10];
+
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    // etc...
+
+    printf("The 2nd number in the array is %d\n", numbers[1]);
+    return 0;
+}
--- variables_and_types/vat.c
+++ variables_and_types/vat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     // calculate with some integers
     int a = 0;
     int b = 1;
